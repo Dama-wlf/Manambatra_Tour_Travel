a30c63a47811377786502ab256daca770a8db3ab
Dama-wlf <damaanil1504@gmail.com>
2026-02-06 23:25:31 +0300

    Mise à jour de App

--- MTT/src/App.jsx
+++ MTT/src/App.jsx
@@ -1,8 +1,22 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import WhyChooseUs from "./components/WhyChooseUs";
+import TourPackages from "./components/TourPackages";
+import About from "./components/About";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
+      <Hero />
+      <Services />
+      <WhyChooseUs />
+      <TourPackages />
+      <About />
+      <ContactForm />
+      <Footer />
     </div>
   );
 }
